53ef2366384e955f3bba94860e05758d11ad647b
khachenkunwong <62020538@up.ac.th>
2021-11-04 16:48:25 +0700

    0.1

--- lib/testgethub_copilot.dart
+++ lib/testgethub_copilot.dart
@@ -25,10 +25,38 @@ class Button_test extends StatelessWidget {
         ),
       ],
     );
-   
-   
-    
   }
 }
+// สร้าง 1 หน้าโดยใช้ stateless widget
+// สร้างหน้านี้โดยใช้ Row ซึ่งมี 3 ส่วนของหน้า
+// สร้างส่วนที่ 1 ของ Column โดยใช้ ElevatedButton ซึ่งมีข้อมูลของส่วนที่ 1 ของ Column
+// สร้างส่วนที่ 2 ของ Column โดยใช้ ElevatedButton ซึ่งมีข้อมูลของส่วนที่ 2 ของ Column
+// สร้างส่วนที่ 3 ของ Column โดยใช้ ElevatedButton ซึ่งมีข้อมูลของส่วนที่ 3 ของ Column
+class Button_test2 extends StatelessWidget {
+  const Button_test2({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    // Column widget with 3 Buttons
+    return Column(
+      children: [
+        // Button 1
+        ElevatedButton(
+          child: Text('Button 1'),
+          onPressed: () {},
+        ),
+        // Button 2
+        ElevatedButton(
+          child: Text('Button 2'),
+          onPressed: () {},
+        ),
+        // Button 3
+        ElevatedButton(
+          child: Text('Button 3'),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
 
